0cbe1716588c83ed39949cd23add15b6f7b072ee
Luke Davis <luke@finesse.host>
2025-02-19 18:37:58 -0600

    Remove "What's New" logic

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,6 @@ import * as vscode from "vscode";
 import { registerShebangCompletionProviders } from "./createShebangCompletionProvider";
 import OutputWindowLogger from "./OutputWindowLogger";
 import { registerHandleSnippetCompletionCommand } from "./handleSnippetCompletionCommand";
-import { showWhatsNewTeaserIfNeeded } from "./whatsNewTeaser";
 
 export function activate(context: vscode.ExtensionContext) {
   const logger = new OutputWindowLogger();
@@ -10,8 +9,6 @@ export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.concat(registerShebangCompletionProviders(context));
   context.subscriptions.push(registerHandleSnippetCompletionCommand(context));
-
-  showWhatsNewTeaserIfNeeded(context);
 }
 
 export function deactivate() {}
